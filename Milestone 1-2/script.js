d2bac6f7063b0b0a7f841a72bf7d4f3b4f486ca0
farataurus <159816668+farataurus@users.noreply.github.com>
2024-11-10 23:33:04 +0500

    Add files via upload

---
+++ Milestone 1-2/script.js
@@ -0,0 +1,10 @@
+var ref_section = document.getElementById('ref');
+var ref_btn = document.getElementById('ref_btn');
+ref_btn === null || ref_btn === void 0 ? void 0 : ref_btn.addEventListener('click', function () {
+    ref_section.classList.toggle('hide');
+});
+var EXP_section = document.getElementById('EXPERIENCE');
+var EXP_btn = document.getElementById('EXP_btn');
+EXP_btn === null || EXP_btn === void 0 ? void 0 : EXP_btn.addEventListener('click', function () {
+    EXP_section.classList.toggle('hide');
+});
